9572efecdd00a0d5c9b0c69d3d5d10dd98a029bb
vellprolol <vellprolol@gmail.com>
2020-10-09 16:44:04 +0300

    Aircrafts genereate and count

---
+++ Main.java
@@ -0,0 +1,13 @@
+import com.skillbox.airport.Aircraft;
+import com.skillbox.airport.Airport;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Airport airport = Airport.getInstance();
+        System.out.println(airport.getAllAircrafts());
+        System.out.println("Всего самолетов " + airport.getAllAircrafts().size());
+    }
+
+}
